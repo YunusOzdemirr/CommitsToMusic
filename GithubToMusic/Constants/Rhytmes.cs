a206232206d6c1b07f6d167830394c4b714850d2
YunusOzdemirr <54598461+YunusOzdemirr@users.noreply.github.com>
2025-01-13 09:59:35 +0300

    Update Rhytmes.cs

--- GithubToMusic/Constants/Rhytmes.cs
+++ GithubToMusic/Constants/Rhytmes.cs
@@ -4,11 +4,11 @@ namespace GithubCommitsToMusic.Constants
 {
     public static class Rhythmes
     {
-        public static Note[] defaultRhythmPattern = new[] { Enums.Note.Do, Enums.Note.Mi, Enums.Note.Sol, Enums.Note.La };
-        public static Note[] populerRhythmPattern = new[] { Enums.Note.Do, Enums.Note.La, Enums.Note.Fa, Enums.Note.Sol };
-        public static Note[] happyRhythmPattern = new[] { Enums.Note.Do, Enums.Note.Sol, Enums.Note.La, Enums.Note.Fa };
-        public static Note[] dramaticRhythmPattern = new[] { Enums.Note.La, Enums.Note.Fa, Enums.Note.Do, Enums.Note.Sol };
-        public static Note[] sadRhythmPattern = new[] { Enums.Note.Sol, Enums.Note.Do, Enums.Note.Fa, Enums.Note.La };
+        public static Note[] defaultRhythmPattern = new[] { Note.Do, Note.Mi, Note.Sol, Note.La };
+        public static Note[] populerRhythmPattern = new[] { Note.Do, Note.La, Note.Fa, Note.Sol };
+        public static Note[] happyRhythmPattern = new[] { Note.Do, Note.Sol, Note.La, Note.Fa };
+        public static Note[] dramaticRhythmPattern = new[] { Note.La, Note.Fa, Note.Do, Note.Sol };
+        public static Note[] sadRhythmPattern = new[] { Note.Sol, Note.Do, Note.Fa, Note.La };
         public static Note[] GetNotesPattern(RhythmPatternType rhytmPatternType)
         {
             switch (rhytmPatternType)
