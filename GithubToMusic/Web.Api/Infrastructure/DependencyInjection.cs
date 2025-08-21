f9ed8a549563d36da204ce3faa959d8e748fd669
Yunus <54598461+YunusOzdemirr@users.noreply.github.com>
2025-08-22 00:28:18 +0300

    Update DependencyInjection.cs

--- GithubToMusic/Web.Api/Infrastructure/DependencyInjection.cs
+++ GithubToMusic/Web.Api/Infrastructure/DependencyInjection.cs
@@ -1,51 +1,58 @@
-﻿using System.Text;
-using Ardalis.GuardClauses;
+﻿using Ardalis.GuardClauses;
 using GithubCommitsToMusic.Infrastructure;
 using GithubCommitsToMusic.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjection
 {
+    
     public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
     {
         var connectionString = builder.Configuration.GetConnectionString("Database");
         Guard.Against.Null(connectionString, message: "Connection string 'Database' not found.");
 
-
-        builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        Console.WriteLine($"--- Database Connection String: {connectionString} ---");
+        try
         {
-            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
-            options.UseSqlServer(connectionString).AddAsyncSeeding(sp);
-        });
+            builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString).AddAsyncSeeding(sp);
+            });
 
 
-        builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+            builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
-        builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+            builder.Services.AddScoped<ApplicationDbContextInitialiser>();
 
 
-        builder.Services.AddAuthorizationBuilder();
+            builder.Services.AddAuthorizationBuilder();
 
-        builder.Services.Configure<IdentityOptions>(options =>
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+            });
+
+            builder.Services.AddSingleton(TimeProvider.System);
+
+            builder.Services.AddAuthorization(options =>
+                options.AddPolicy("CanPurge", policy => policy.RequireRole("Administrator")));
+
+            builder.Services.AddOpenApi();
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+
+        }
+        catch (Exception)
         {
-            options.Password.RequireDigit = true;
-            options.Password.RequiredLength = 6;
-        });
 
-        builder.Services.AddSingleton(TimeProvider.System);
-
-        builder.Services.AddAuthorization(options =>
-            options.AddPolicy("CanPurge", policy => policy.RequireRole("Administrator")));
-
-        builder.Services.AddOpenApi();
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
+            throw;
+        }
     }
 }
