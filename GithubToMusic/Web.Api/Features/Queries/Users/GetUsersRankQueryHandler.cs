76589451f99e900191793e5eb8aeb504f6690e13
Yunus <54598461+YunusOzdemirr@users.noreply.github.com>
2025-08-23 23:32:51 +0300

    leaderboard

---
+++ GithubToMusic/Web.Api/Features/Queries/Users/GetUsersRankQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using GithubCommitsToMusic.Enums;
+using GithubCommitsToMusic.Interfaces;
+using GithubCommitsToMusic.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GithubCommitsToMusic.Features.Queries.Users
+{
+    public class GetUsersRankQueryHandler(IApplicationDbContext applicationDbContext) : IRequestHandler<GetUsersRankQuery, IList<User>>
+    {
+        public async Task<IList<User>> Handle(GetUsersRankQuery request, CancellationToken cancellationToken)
+        {
+            var users = await applicationDbContext.Users.Include(a => a.Commits).ToListAsync(cancellationToken);
+            if (users == null || users.Count == 0)
+                return null;
+            if (request.OrderBy == OrderBy.Ascending)
+            {
+                return users.OrderByDescending(a => a.CreatedOn).ToList();
+            }
+            else
+            {
+                return users.OrderBy(a => a.Commits.Count).ToList();
+            }
+        }
+    }
+}
